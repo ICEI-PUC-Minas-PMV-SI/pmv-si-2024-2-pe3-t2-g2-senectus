837162bf0d55f5d4fe610f74da1ac37d0eb7cecc
NicolasCBV <nicolascbv700@gmail.com>
2024-10-29 17:16:39 -0300

    feat(consultas-idoso): adicionar pagina de consulta para idoso

---
+++ src/app/appointments/page.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { AppContainer } from '@components/common/Container/AppContainer'
+import { AppDashboardHeader } from '@components/common/DashboardHeader/AppDashboardHeader'
+import { AppInitialText } from '@components/common/Text/AppInitialText'
+import { NextUIProvider } from '@nextui-org/react'
+import { FaMagnifyingGlass } from 'react-icons/fa6'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '@themes/theme'
+import { AppAppointmentsRoutineCalendar } from '@components/appointments/routine/AppAppointmentsRoutineCalendar'
+import {
+  AppointmentsEntity,
+  AppointmentStateEnum
+} from '@core/models/AppointmentsEntity'
+import { RandomAppointmentsSeedService } from '@core/services/seed/clients/RandomAppointmentsSeedService'
+import { useCalendar } from '../../hooks/useCalendar'
+import { useEffect } from 'react'
+import { AppHeader } from '@components/common/Header/AppHeader'
+
+export default function AppointmentsScreen() {
+  const [appointments, setAppointments] = useCalendar<AppointmentsEntity>()
+
+  useEffect(() => {
+    const accepted = RandomAppointmentsSeedService.exec(
+      100,
+      AppointmentStateEnum.PENDENT
+    )
+    const pendent = RandomAppointmentsSeedService.exec(
+      100,
+      AppointmentStateEnum.ACCEPTED
+    )
+
+    setAppointments([...accepted, ...pendent])
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <NextUIProvider className="default">
+        <AppHeader />
+
+        <AppContainer style={{ justifyContent: 'start' }}>
+          <AppDashboardHeader
+            options={[
+              {
+                name: 'Nova consulta',
+                description: 'Solicite uma nova consulta',
+                href: '/appointments/new',
+                icon: <FaMagnifyingGlass />
+              }
+            ]}
+          />
+
+          <AppInitialText
+            title="Consultas"
+            text="Confira as suas consultas pelo nosso calendário:"
+          />
+
+          <AppAppointmentsRoutineCalendar appointments={appointments} />
+        </AppContainer>
+      </NextUIProvider>
+    </ThemeProvider>
+  )
+}
--- src/components/common/Modals/AppModal.tsx
+++ src/components/common/Modals/AppModal.tsx
@@ -7,28 +7,35 @@ import {
   useDisclosure
 } from '@nextui-org/modal'
 
+export interface ModalControllerProps {
+  isOpen: boolean
+  onOpen: () => void
+  onOpenChange: () => void
+  onClose: () => void
+}
+
 interface AppModalProps extends ComponentProps<'div'> {
   title: string
   children: ReactNode
   modalBody: ReactNode
+  controller?: ModalControllerProps
 }
 
-export function AppModal({
+function AppInternalModal({
   title,
   children,
   modalBody,
+  controller,
   ...props
-}: AppModalProps) {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure()
-
+}: Replace<AppModalProps, { controller: ModalControllerProps }>) {
   return (
     <>
-      <div role="button" onClick={onOpen} {...props}>
+      <div role="button" onClick={controller.onOpen} {...props}>
         {children}
       </div>
       <Modal
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
+        isOpen={controller.isOpen}
+        onOpenChange={controller.onOpenChange}
         placement="bottom-center"
         size="xs"
       >
@@ -38,5 +45,36 @@ export function AppModal({
         </ModalContent>
       </Modal>
     </>
+  )
+}
+
+export function AppModal({
+  title,
+  children,
+  modalBody,
+  controller,
+  ...props
+}: AppModalProps) {
+  const disclosure = useDisclosure()
+  if (!controller)
+    return (
+      <AppInternalModal
+        title={title}
+        modalBody={modalBody}
+        controller={disclosure}
+        {...props}
+      >
+        {children}
+      </AppInternalModal>
+    )
+  return (
+    <AppInternalModal
+      title={title}
+      modalBody={modalBody}
+      controller={controller}
+      {...props}
+    >
+      {children}
+    </AppInternalModal>
   )
 }
