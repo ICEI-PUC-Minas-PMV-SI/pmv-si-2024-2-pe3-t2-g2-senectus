ecb95281dc68cfc36425b5c0dbcd21c72720a068
NicolasCBV <nicolascbv700@gmail.com>
2024-10-31 17:59:27 -0300

    fix(consultas-idoso): corrigir responsividade e input filter search

--- src/components/professional_card/ProfessionalCard.tsx
+++ src/components/professional_card/ProfessionalCard.tsx
@@ -1,77 +1,90 @@
-import { AppButtonActionRect } from "@components/common/Buttons/AppButtonActionRect";
+import { AppButtonActionRect } from '@components/common/Buttons/AppButtonActionRect'
 import {
-    ProfessionalCardStyle,
-    ProfessionalItemIconStyle,
-    ProfessionalItemStyle,
-    ProfessionalRoleStyle,
-    ProfessionalsItemsStyle
-} from "./ProfessionalCardStyles";
-import { FaMapMarkerAlt, FaPhone, FaEnvelope } from "react-icons/fa";
-import { FaCalendarDays } from "react-icons/fa6";
-import ProfessionalEntity from "@core/models/ProfessionalEntity";
+  ProfessionalCardStyle,
+  ProfessionalItemIconStyle,
+  ProfessionalItemStyle,
+  ProfessionalRoleStyle,
+  ProfessionalsItemsStyle
+} from './ProfessionalCardStyles'
+import { FaMapMarkerAlt, FaPhone, FaEnvelope } from 'react-icons/fa'
+import { FaCalendarDays } from 'react-icons/fa6'
+import ProfessionalEntity from '@core/models/ProfessionalEntity'
 
 interface ProfessionalCardProps {
-    professional: ProfessionalEntity;
+  professional: ProfessionalEntity
 }
 
 interface ProfessionalCardItemProps {
-    type: ProfessionalCardItemType;
-    text: string;
+  type: ProfessionalCardItemType
+  text: string
 }
 
 enum ProfessionalCardItemType {
-    EMAIL,
-    PHONE,
-    CITY
+  EMAIL,
+  PHONE,
+  CITY
 }
 
-export default function ProfessionalCard({ professional }: ProfessionalCardProps) {
-    return (
-        <ProfessionalCardStyle>
-            <ProfessionalRoleStyle>
-                <p style={{ fontSize: "1rem "}}>{professional.role}</p>
-            </ProfessionalRoleStyle>
+export default function ProfessionalCard({
+  professional
+}: ProfessionalCardProps) {
+  return (
+    <ProfessionalCardStyle>
+      <ProfessionalRoleStyle>
+        <p>{professional.role}</p>
+      </ProfessionalRoleStyle>
 
-            <ProfessionalsItemsStyle>
-                <h6 style={{ fontSize: "1.2rem", marginBottom: "4px" }}>
-                    {professional.name}
-                </h6>
+      <ProfessionalsItemsStyle>
+        <h6 style={{ fontSize: '1.2rem', marginBottom: '4px' }}>
+          {professional.name}
+        </h6>
 
-                <ProfessionalCardItem type={ProfessionalCardItemType.EMAIL} text={professional.email} />
-                <ProfessionalCardItem type={ProfessionalCardItemType.PHONE} text={professional.phone} />
-                <ProfessionalCardItem type={ProfessionalCardItemType.CITY} text={professional.city} />
+        <ProfessionalCardItem
+          type={ProfessionalCardItemType.EMAIL}
+          text={professional.email}
+        />
+        <ProfessionalCardItem
+          type={ProfessionalCardItemType.PHONE}
+          text={professional.phone}
+        />
+        <ProfessionalCardItem
+          type={ProfessionalCardItemType.CITY}
+          text={professional.city}
+        />
 
-                <div style={{ marginTop: "8px" }}>
-                    <AppButtonActionRect fullWidth={true}  text="Contatar" icon={<FaCalendarDays />} />
-                </div>
-            </ProfessionalsItemsStyle>
-        </ProfessionalCardStyle>
-    );
+        <div style={{ marginTop: '8px' }}>
+          <AppButtonActionRect
+            fullWidth={true}
+            text="Contatar"
+            icon={<FaCalendarDays />}
+          />
+        </div>
+      </ProfessionalsItemsStyle>
+    </ProfessionalCardStyle>
+  )
 }
 
 function ProfessionalCardItem({ type, text }: ProfessionalCardItemProps) {
-    let icon;
+  let icon
 
-    switch (type) {
-        case ProfessionalCardItemType.EMAIL:
-            icon = <FaEnvelope />;
-            break;
-        case ProfessionalCardItemType.PHONE:
-            icon = <FaPhone />;
-            break;
-        case ProfessionalCardItemType.CITY:
-            icon = <FaMapMarkerAlt />;
-            break;
-        default:
-            icon = null;
-    }
+  switch (type) {
+    case ProfessionalCardItemType.EMAIL:
+      icon = <FaEnvelope />
+      break
+    case ProfessionalCardItemType.PHONE:
+      icon = <FaPhone />
+      break
+    case ProfessionalCardItemType.CITY:
+      icon = <FaMapMarkerAlt />
+      break
+    default:
+      icon = null
+  }
 
-    return (
-        <ProfessionalItemStyle>
-            <ProfessionalItemIconStyle>
-                {icon}
-            </ProfessionalItemIconStyle>
-            <p style={{ marginLeft: "8px", fontSize: "0.8rem" }}>{text}</p>
-        </ProfessionalItemStyle>
-    );
+  return (
+    <ProfessionalItemStyle>
+      <ProfessionalItemIconStyle>{icon}</ProfessionalItemIconStyle>
+      <p style={{ marginLeft: '8px', fontSize: '0.8rem' }}>{text}</p>
+    </ProfessionalItemStyle>
+  )
 }
