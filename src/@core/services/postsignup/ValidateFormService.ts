7d9f24de12d2ffaea5cccea59e1786fa765ed752
Júlia Mascari <129827576+juliamascari@users.noreply.github.com>
2024-11-03 03:08:07 -0300

    Add files via upload

---
+++ src/@core/services/postsignup/ValidateFormService.ts
@@ -0,0 +1,39 @@
+import { PostSignUpFormProps } from '@components/postsignup/AppFormPostSignUp'
+import { formatZodError } from '@core/utils/formatError'
+import { z, ZodError } from 'zod'
+
+// Agrega-se todos os dados do formulário junto com o atributo isError
+type PostSignUpFormPropsReturn = PostSignUpFormProps & { isError?: boolean }
+
+export class ValidateFormService {
+  static exec(objt: PostSignUpFormProps): PostSignUpFormPropsReturn {
+    const schema = z.object({
+      name: z
+        .string()
+        .email('Nome inválido')
+        .min(3, 'Nome precisa ter no mínimo 2 caracteres')
+        .max(320, 'Nome precisa ter no máximo 320 caracteres'),  
+      email: z
+        .string()
+        .email('Email inválido')
+        .min(3, 'Email precisa ter no mínimo 2 caracteres')
+        .max(320, 'Email precisa ter no máximo 320 caracteres'),
+      password: z
+        .string()
+        .min(6, 'Senha precisa ter no mínimo 6 caracteres')
+        .max(100, 'Senha precisa ter no máximo 100 caracteres')
+    })
+
+    try {
+      const res = schema.parse(objt)
+      return res
+    } catch (e) {
+      // formatZodError vai coletar os erros definidos no esquema acima
+      const formattedError = formatZodError(objt, e as ZodError)
+      return {
+        ...formattedError,
+        isError: true
+      }
+    }
+  }
+}
