919acbb0348cd4af1acd51bb345bd62ef3204d62
João Pedro Reis Martins <joaopedrormartins01@gmail.com>
2024-11-04 00:14:37 -0300

    Add files via upload

---
+++ src/app/blogs/[id]/page.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../../themes/theme';
+import { NextUIProvider } from '@nextui-org/react';
+import { AppHeader } from '@components/common/Header/AppHeader';
+import { AppContainer } from '@components/common/Container/AppContainer';
+import { PageContainer } from '@components/blogs/details/PageContainerStyle';
+import { HeaderSection } from '@components/blogs/details/HeaderSectionStyle';
+import { ContentSection } from '@components/blogs/details/ContentSectionStyle';
+import { PostInfo } from '@components/blogs/details/PostInfoStyle';
+import { Sidebar } from '@components/blogs/details/SidebarStyle';
+import { Card } from '@components/blogs/details/CardStyle';
+import '../../globals.css';
+
+const posts = [
+  {
+    id: "1",
+    title: "Como a Atividade Física Melhora a Qualidade de Vida na Terceira Idade",
+    image: "/img/blogs/post-1.jpg",
+    trustLevel: "Confiável",
+    viewsCount: "2K visualizações",
+    description: `
+      A prática regular de atividades físicas é essencial para o envelhecimento saudável. Estudos mostram que o exercício físico pode ajudar a prevenir doenças crônicas como diabetes, hipertensão e problemas cardíacos. Além disso, a atividade física melhora o humor, combate a depressão e a ansiedade, promovendo uma sensação de bem-estar geral.
+
+      Pesquisas indicam que o exercício também contribui para a preservação das habilidades cognitivas, reduzindo o risco de demência. Atividades físicas regulares, como caminhadas e alongamentos, ajudam a fortalecer os músculos e a melhorar o equilíbrio, o que é fundamental para a prevenção de quedas — uma das maiores causas de hospitalizações entre idosos.
+
+      Além disso, a prática constante de exercícios pode melhorar a mobilidade e a flexibilidade, permitindo que os idosos mantenham a independência em suas atividades diárias. Com todos esses benefícios, incorporar a atividade física na rotina é um dos passos mais importantes para promover uma vida saudável e ativa na terceira idade.
+    `
+  },
+  {
+    id: "2",
+    title: "Caminhada: O Exercício Ideal para Idosos",
+    image: "/img/blogs/post-2.jpg",
+    trustLevel: "Confiável",
+    viewsCount: "1.5K visualizações",
+    description: `
+      A caminhada é uma das atividades físicas mais acessíveis e benéficas para idosos. Por ser uma atividade de baixo impacto, é uma excelente escolha para manter a saúde cardiovascular sem exigir grande esforço físico. Caminhar regularmente ajuda a fortalecer o coração, melhorar a circulação sanguínea e controlar a pressão arterial.
+
+      Além dos benefícios físicos, a caminhada proporciona momentos de socialização, especialmente quando realizada em grupo, incentivando a interação e reduzindo o isolamento social, que é comum na terceira idade. O contato com a natureza durante caminhadas em parques ou praças pode ser revigorante e ajudar a combater o estresse.
+
+      Caminhadas diárias, mesmo que curtas, podem aumentar a disposição e promover uma noite de sono mais tranquila. Para muitos idosos, caminhar é mais do que uma atividade física — é um ritual que melhora a qualidade de vida de forma holística.
+    `
+  },
+  {
+    id: "3",
+    title: "Dicas de Exercícios Adaptados para Idosos",
+    image: "/img/blogs/post-3.jpg",
+    trustLevel: "Confiável",
+    viewsCount: "1.3K visualizações",
+    description: `
+      Exercícios adaptados são fundamentais para atender às necessidades físicas de idosos, proporcionando segurança e eficácia. Alongamentos, fortalecimento muscular e exercícios de equilíbrio são especialmente úteis para melhorar a mobilidade e reduzir o risco de quedas, um problema comum nessa fase da vida.
+
+      A prática de exercícios adaptados pode incluir atividades como levantar-se de uma cadeira sem apoio, que trabalha a força das pernas, e equilíbrio em um pé, que fortalece o core. Exercícios de resistência, como o uso de faixas elásticas, ajudam a preservar a força muscular, enquanto exercícios de respiração são eficazes para aumentar a capacidade pulmonar.
+
+      Esses exercícios adaptados não apenas melhoram a força física, mas também proporcionam confiança e maior independência para realizar atividades do cotidiano. Com a orientação de um profissional, é possível adaptar a rotina de exercícios de acordo com as limitações e necessidades individuais.
+    `
+  },
+  {
+    id: "4",
+    title: "Importância do Acompanhamento Médico na Terceira Idade",
+    image: "/img/blogs/post-4.jpg",
+    trustLevel: "Confiável",
+    viewsCount: "1.2K visualizações",
+    description: `
+      O acompanhamento médico regular é essencial para garantir a saúde e o bem-estar na terceira idade. Consultas frequentes ajudam a identificar precocemente doenças comuns, como osteoporose, problemas cardíacos e diabetes, permitindo tratamentos mais eficazes.
+
+      Muitos idosos enfrentam o desafio de administrar múltiplas condições de saúde ao mesmo tempo. Com o acompanhamento médico adequado, é possível ajustar medicamentos e tratamentos para reduzir efeitos colaterais e otimizar a saúde. Exames de rotina, como check-ups cardiovasculares e exames de densidade óssea, também são essenciais para a prevenção de complicações.
+
+      O apoio psicológico durante as consultas também é crucial, pois a saúde mental afeta diretamente a saúde física. Ter profissionais de confiança e um plano de cuidados bem estruturado garante uma qualidade de vida mais elevada e aumenta a longevidade.
+    `
+  },
+  {
+    id: "5",
+    title: "Dicas de Alimentação Saudável para Idosos",
+    image: "/img/blogs/post-5.jpg",
+    trustLevel: "Confiável",
+    viewsCount: "1.8K visualizações",
+    description: `
+      Uma alimentação saudável é crucial para manter a saúde e a energia na terceira idade. Nessa fase, é importante consumir alimentos ricos em nutrientes, como frutas, legumes, proteínas magras e grãos integrais, que fornecem vitaminas e minerais essenciais para o corpo.
+
+      Um aspecto importante da nutrição na terceira idade é a ingestão adequada de fibras para promover a saúde digestiva e prevenir a constipação, comum entre idosos. A hidratação é igualmente essencial, pois a sensação de sede tende a diminuir com o passar dos anos, aumentando o risco de desidratação.
+
+      Evitar alimentos processados, açúcares refinados e excesso de sódio é fundamental para o controle de condições como hipertensão e diabetes. Pequenas mudanças na alimentação, com a ajuda de um nutricionista, podem fazer uma grande diferença na qualidade de vida e bem-estar diário dos idosos.
+    `
+  }
+];
+
+
+export default function BlogPostDetailsPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return <p>Postagem não encontrada.</p>;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <NextUIProvider className="default">
+        <AppHeader />
+        <AppContainer>
+          <PageContainer>
+            <HeaderSection>
+              <h1 className="prettify-title">{post.title}</h1>
+            </HeaderSection>
+
+            <ContentSection>
+              <PostInfo>
+                <img src={post.image} alt={post.title} />
+                <hr />
+                <div className="description">
+                  <p>{post.description}</p>
+                </div>
+              </PostInfo>
+
+              <Sidebar>
+                <Card>
+                  <p>✓ {post.trustLevel}</p>
+                  <div className="separator"></div>
+                  <div className="interest-info">
+                    <svg className="eye-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                      <path d="M12 5C7 5 2.73 8.11 1 12c1.73 3.89 6 7 11 7s9.27-3.11 11-7c-1.73-3.89-6-7-11-7zm0 12c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+                    </svg>
+                    <p>{post.viewsCount}</p>
+                  </div>
+                </Card>
+              </Sidebar>
+            </ContentSection>
+          </PageContainer>
+        </AppContainer>
+      </NextUIProvider>
+    </ThemeProvider>
+  );
+}
---
+++ src/app/events/page.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../themes/theme';
+import { NextUIProvider } from '@nextui-org/react';
+import { AppHeader } from '@components/common/Header/AppHeader';
+import { AppContainer } from '@components/common/Container/AppContainer';
+import { AppSearchAndFilter } from '@components/common/Inputs/SearchAndFilter/AppSearchAndFilter';
+import { AppInitialText } from '@components/common/Text/AppInitialText';
+import { useState } from 'react';
+import { AppPagination } from '@components/common/Pagination/AppPagination';
+import { EventAvatar } from '@components/events/list/EventAvatarStyle';
+import { EventInfo } from '@components/events/list/EventInfoStyle';
+import { EventItemContainer  } from '@components/events/list/EventItemContainerStyle';
+import '../globals.css';
+
+const events = [
+  {
+    id: 1,
+    name: "Marcos Lessa",
+    role: "Geriatra",
+    title: "Saúde em Movimento: Caminhada e Alongamento",
+    description: "Atividades ao ar livre com caminhada leve e exercícios de alongamento.",
+    time: "1 hora atrás"
+  },
+  {
+    id: 2,
+    name: "Carla Ferreira",
+    role: "Fisioterapeuta",
+    title: "Encontro de Pilates para Idosos",
+    description: "Aula de pilates adaptado para fortalecer músculos e melhorar a flexibilidade.",
+    time: "3 horas atrás"
+  },
+  {
+    id: 3,
+    name: "Antônio Almeida",
+    role: "Educador Físico",
+    title: "Aula de Dança para Idosos",
+    description: "Uma sessão divertida de dança de baixo impacto para socializar e manter-se ativo.",
+    time: "5 horas atrás"
+  },
+  {
+    id: 4,
+    name: "Silvia Mendes",
+    role: "Nutricionista",
+    title: "Oficina de Alimentação Saudável para a Terceira Idade",
+    description: "Ensinando a montar refeições nutritivas para saúde e energia diárias.",
+    time: "7 horas atrás"
+  },
+  {
+    id: 5,
+    name: "Pedro Lima",
+    role: "Cardiologista",
+    title: "Palestra sobre Saúde Cardíaca",
+    description: "Dicas práticas para cuidar do coração na terceira idade e prevenir doenças.",
+    time: "9 horas atrás"
+  }
+];
+
+export default function EventSearchPage() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const options = ['Todos', 'Caminhada', 'Pilates'];
+
+  const handleSearchChange = (value: string) => {
+    setSearchTerm(value);
+  };
+
+  const handleFilterClick = (selectedOption: string) => {
+    console.log(`Filtro selecionado: ${selectedOption}`);
+  };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <NextUIProvider className="default">
+        <AppHeader />
+
+        <AppContainer style={{ justifyContent: 'start' }}>
+          <AppInitialText 
+            title="Sua lista de eventos" 
+            subtitle="Pesquise o melhor evento para melhorar sua saúde." 
+          />
+          <AppSearchAndFilter
+            onFilterClick={handleFilterClick}
+            onChange={handleSearchChange}
+            placeholder="Insira o nome do evento"
+            options={options}
+          />
+
+          <section className="event-list">
+            {events.map((event) => (
+              <EventItemContainer key={event.id} href={`/events/${event.id}`}>
+                <EventAvatar>{event.name[0]}</EventAvatar>
+                <EventInfo>
+                  <div className="header-row">
+                    <div className="name-and-role">
+                      <h3>{event.name}</h3>
+                      <p className="role">{event.role}</p>
+                    </div>
+                    <span className="timestamp">{event.time}</span>
+                  </div>
+                  <p className="title">{event.title}</p>
+                  <p className="description">{event.description}</p>
+                </EventInfo>
+              </EventItemContainer>
+            ))}
+          </section>
+
+          {/* Paginação */}
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <AppPagination 
+              total={5} 
+              initialPage={1} 
+              onChange={(page) => handlePageChange(page)} 
+            />
+          </div>
+
+        </AppContainer>
+      </NextUIProvider>
+    </ThemeProvider>
+  );
+}
