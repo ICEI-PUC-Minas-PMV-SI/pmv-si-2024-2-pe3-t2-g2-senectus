dfc3df013001acaffe2b76c2908dc5da18f98cb7
Júlia Mascari <129827576+juliamascari@users.noreply.github.com>
2024-11-04 23:09:38 -0300

    alteração - ValidateFormService.ts

--- src/@core/services/signup/ValidateFormService.ts
+++ src/@core/services/signup/ValidateFormService.ts
@@ -11,17 +11,17 @@ export class ValidateFormService {
       name: z
         .string()
         .email('Nome inválido')
-        .min(3, 'Nome precisa ter no mínimo 2 caracteres')
-        .max(320, 'Nome precisa ter no máximo 320 caracteres'),  
+        .min(2, 'Nome precisa ter no mínimo 2 caracteres')
+        .max(80, 'Nome precisa ter no máximo 80 caracteres'),  
       email: z
         .string()
         .email('Email inválido')
-        .min(3, 'Email precisa ter no mínimo 2 caracteres')
-        .max(320, 'Email precisa ter no máximo 320 caracteres'),
+        .min(2, 'Email precisa ter no mínimo 2 caracteres')
+        .max(40, 'Email precisa ter no máximo 40 caracteres'),
       password: z
         .string()
         .min(6, 'Senha precisa ter no mínimo 6 caracteres')
-        .max(100, 'Senha precisa ter no máximo 100 caracteres')
+        .max(50, 'Senha precisa ter no máximo 50 caracteres')
     })
 
     try {
