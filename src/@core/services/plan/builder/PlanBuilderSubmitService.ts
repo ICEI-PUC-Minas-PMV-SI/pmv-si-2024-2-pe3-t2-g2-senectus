a4fa1c623af624cc4ec56f4f7ca2ef88d08d9c85
NicolasCBV <nicolascbv700@gmail.com>
2024-11-07 17:44:12 -0300

    fix(logica): corrigir calculo de progresso no plano de treino

--- src/@core/services/plan/builder/PlanBuilderSubmitService.ts
+++ src/@core/services/plan/builder/PlanBuilderSubmitService.ts
@@ -1,11 +1,15 @@
 import { TrainingPlanEntity } from '@core/models/TrainingPlanEntity'
 import { TrainingPlansRepo } from '@core/repositories/TrainingPlansRepo'
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime'
+import { GetPlanProgressService } from '../crud/GetPlanProgressService'
 
 export class PlanBuilderSubmitService {
   static exec(router: AppRouterInstance, plan: TrainingPlanEntity) {
     const clone = plan.clone()
     clone.stackHolderRef = undefined
+
+    const { progress } = GetPlanProgressService.exec(clone)
+    clone.progress = progress // Recalcula o progresso no plano de treino que o usuário fez, caso o mesmo já exista
 
     TrainingPlansRepo.set(clone)
     router.push('/clients', { scroll: true })
