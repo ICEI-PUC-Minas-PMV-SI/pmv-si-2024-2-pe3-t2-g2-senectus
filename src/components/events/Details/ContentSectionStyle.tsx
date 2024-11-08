6fe1b41afc43a8cb9140509ab4516490178a0362
NicolasCBV <nicolascbv700@gmail.com>
2024-11-08 12:51:05 -0300

    fix(blog e eventos): corrigir erros que impedem o build

---
+++ src/components/events/Details/ContentSectionStyle.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const ContentSection = styled.div`
+  display: flex;
+  width: fit-content;
+  gap: 20px;
+  align-items: flex-start;
+`
---
+++ src/components/events/Details/PageContainerStyle.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  margin: 40px auto;
+`
