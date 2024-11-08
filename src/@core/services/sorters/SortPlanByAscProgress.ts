fb6aa51460c6fe2c1db505f1862080051e8d0053
NicolasCBV <nicolascbv700@gmail.com>
2024-11-08 02:22:11 -0300

    feat(logica): adicionar regras de negocio no fluxo de cadastro

--- src/@core/services/sorters/SortPlanByAscProgress.ts
+++ src/@core/services/sorters/SortPlanByAscProgress.ts
@@ -1,4 +1,5 @@
-export function sortInAsc(c1: object, c2: object) {
+type Arg = object | number | string
+export function sortInAsc(c1: Arg, c2: Arg) {
   if (c1 < c2) return -1
   if (c1 > c2) return 1
   return 0
